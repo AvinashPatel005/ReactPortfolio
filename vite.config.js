e2116ee60abdd1b288cc862f3f5530c2ffbe311e
Avinash <avinashpatel2241016220@gmail.com>
2025-02-13 14:40:39 +0530

    Pwa Manifest Updated

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,33 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
 
-    plugins: [VitePWA(),react()], 
+    plugins: [VitePWA(
+      {
+        "short_name": "Portfolio",
+        "name": "Avinash' Portfolio",
+        "icons": [
+          {
+            "src": "logo.png",
+            "sizes": "64x64 32x32 24x24 16x16",
+            "type": "image/x-icon"
+          },
+          {
+            "src": "logo.png",
+            "type": "image/png",
+            "sizes": "192x192"
+          },
+          {
+            "src": "logo.png",
+            "type": "image/png",
+            "sizes": "512x512"
+          }
+        ],
+        "start_url": ".",
+        "display": "standalone",
+        "theme_color": "#ffffff",
+        "background_color": "#000000"
+      }
+      
+    ),react()], 
 
 });
